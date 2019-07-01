5450a3ab514fc1342fb8576a939cd2269294f6f0
root <root@mars.(none)>
2019-07-01 17:36:20 +0900

    update

--- main.py
+++ main.py
@@ -3,29 +3,122 @@
 
 import sys					# system by core
 import re					# regular expression by core
-import tdb 
+import tdb
 import config
 import time
 import mt
 
+def move_to_run_table(db_conn, hash_key, title):
+    if not db_conn:
+        print("Please Check DB Connection")
+        return ''
+
+    runp = tdb.run_tbl()
+    runp.set_db_connection(db_conn)
+    r_ret, r_result_set = runp.torr_run_search(hash_key)
+    if not r_result_set:
+        ctp = tdb.candi_tbl()
+        ctp.set_db_connection(db_conn)
+        c_ret, c_result_set = ctp.torr_candi_search(hash_key)
+        if c_result_set:
+            ctl = ctp.torr_candi_fill_result(c_result_set[0])
+            runtl = tdb.run_tbl_t(ctl.hash_magnet, ctl.title, int(time.time()),
+                                1, ctl.category, ctl.sitename)
+        else:
+            runtl = tdb.run_tbl_t(hash_key, title, int(time.time()),
+                            1, 'Unknown', 'Unknown')
+
+        runtp = tdb.run_tbl()
+        runtp.set_db_connection(db_conn)
+        runtp.torr_run_create(runtl)
+    else:
+        ctp = tdb.candi_tbl()
+        ctp.set_db_connection(db_conn)
+        ctp.torr_candi_delete(hash_key)
+
+def move_to_complete_table(db_conn, hash_key, title):
+    if not db_conn:
+        print("Please Check DB Connection")
+        return ''
+
+    comp = tdb.ret_tbl()
+    comp.set_db_connection(db_conn)
+    runtp = tdb.run_tbl()
+    runtp.set_db_connection(db_conn)
+    ctp = tdb.candi_tbl()
+    ctp.set_db_connection(db_conn)
+
+    com_ret, com_result_set = comp.torr_ret_search(hash_key)
+    if not com_result_set:
+        run_ret, run_result_set = runtp.torr_run_search(hash_key)
+        if not run_result_set:
+            c_ret, c_result_set = ctp.torr_candi_search(hash_key)
+            if not c_result_set:
+                comtl = tdb.ret_tbl_t(hash_key, title, int(time.time()),
+                                100, 'Unknown', 'Unknown')
+            else:
+                ctl = ctp.torr_candi_fill_result(c_result_set[0])
+                comtl = tdb.ret_tbl_t(ctl.hash_magnet, ctl.title, int(time.time()),
+                                100, ctl.category, ctl.sitename)
+        else:
+            runtl = runtp.torr_run_fill_result(run_result_set[0])
+            comtl = tdb.ret_tbl_t(runtl.hash_magnet, runtl.title, int(time.time()),
+                                100, runtl.category, runtl.sitename)
+        comp.torr_ret_create(comtl)
+
+    # clear romplete item in candi and run table.
+    ctp.torr_candi_delete(hash_key)
+    runtp.torr_run_delete(hash_key)
+
+def get_old_item_from_candi_tbl(db_conn):
+    if not db_conn:
+        print("Please Check DB Connection")
+        return {}
+
+    ctp = tdb.candi_tbl()
+    ctp.set_db_connection(db_conn)
+    ret, result_set = ctp.torr_candi_search_old_item()
+    if result_set:
+        #print("GET OLD ONE FROM CANDI ({})".format(ctp.torr_candi_fill_result(result_set[0])))
+        print("GET OLD ONE FROM CANDI ")
+        return result_set[0]
+    return {}
+
+def get_request_candidate(db_conn):
+    candi_dic = get_old_item_from_candi_tbl(db_conn)
+    if candi_dic:
+        return candi_dic['HASH']
+    return None
+
 def main():
 	c_ini = config.TwalConfig('./config.ini', debug=False)
 	db_conn = tdb.connect_to_db(
-			c_ini.DB.db_address, 
-			c_ini.DB.db_port, 
-			c_ini.DB.db_user_id, 
-			c_ini.DB.db_user_pass, 
+			c_ini.DB.db_address,
+			c_ini.DB.db_port,
+			c_ini.DB.db_user_id,
+			c_ini.DB.db_user_pass,
 			c_ini.DB.db_name)
 	if db_conn:
 		print("DB connected")
 	else:
 		print("DB NOTTTTTTT connected")
-	
-	mt.proc_run('', 
-			c_ini.TR.host,
-			c_ini.TR.t_id,
-			c_ini.TR.t_port,
-			t_pass)
+
+    # get magnet from CANDI_TBL
+	candi_magnet_one = get_request_candidate(db_conn)
+	print(candi_magnet_one)
+    # try add to transmission
+	r_dic = {}
+	c_dic = {}
+	running_cnt = 0
+	r_dic, c_dic, running_cnt = mt.proc_run(candi_magnet_one,
+		c_ini.TR.host,
+		c_ini.TR.t_id,
+		c_ini.TR.t_port,
+		c_ini.TR.t_pass)
+	for run_magnet in r_dic.keys():
+		move_to_run_table(db_conn, run_magnet, r_dic[run_magnet]['title'])
+	for com_magnet in c_dic.keys():
+		move_to_complete_table(db_conn, com_magnet, c_dic[com_magnet]['title'])
 
 
 if __name__ in ("__main__"):
--- mt.py
+++ mt.py
@@ -41,7 +41,7 @@ def get_torrent_info(tc):
 		d_time = (datetime.now() - item.date_started).seconds
 		ratio  = item.ratio
 		il.append({'id':id_num, 'status':status, 'name':name, 'hash_string':hash_s, 'delta_time':d_time, 'ratio':ratio})
-		
+
 	return il
 
 def clear_complete_torrent(tr_id, tr_pass, tr_port):
@@ -72,14 +72,13 @@ def remove_torrent_and_data_from_magnet(magnet):
 	u_pass = "sjrnfl"
 	port = 9091
 	tc = None
-	
-	tc = connect_to_server(u_id, u_pass, port) 
+
+	tc = connect_to_server(u_id, u_pass, port)
 	try:
 		li = get_torrent_info(tc)
 	except:
 		print("get_torrent_info exception in remove torrent")
 		print("Try to restart transmission service from remove torrent")
-		os.system('/root/bin/stopTM.sh')
 		return None
 
 	if not li:
@@ -118,42 +117,39 @@ def proc_run(magnet, host=None, t_id=None, t_port=None, t_pass=None):
 		return ''
 	else:
 		print("Connected Transmission server")
-	    return ''
 	try:
 		item_list = get_torrent_info(conn)
 	except:
 		print("get_torrent_info exception")
 		print("Try to stop transmission service")
-		os.system('/root/bin/stopTM.sh')
-		time.sleep(3)
-		print("Try to start transmission serverice")
-		os.system('/root/bin/startTM.sh')
 		return {}, {}, max_cnt
 
 	print(datetime.today())
+	print(item_list)
 	del_cnt = 0
 	magnet_hash_code = magnet.split(':')[-1]
 	try:
 		for item in item_list:
 			print("info {} {} {} {}".format(item['id'], item['status'], item['name'], item['hash_string']))
-			full_hash_magnet = "magnet:?xt=urn:btih:"+item['hash_string'].upper()
+            #full_hash_magnet = "magnet:?xt=urn:btih:"+item['hash_string'].upper()
+			full_hash_magnet = Item['hash_string'].upper()
 			title = item['name']
 			status = item['status']
 			delta_time = item['delta_time']
 			ratio = item['ratio']
-			
+
 			if status == 'seeding' or title.find('360p') >= 0:
 				print("Download Done {} Remove item {}".format(item['name'], datetime.today()))
 				completed_dic[full_hash_magnet]={'title':item['name'],'status':status}
 				remove_torrent_at_server(conn, item['id'])
-				del_cnt += 1 
+				del_cnt += 1
 			elif ratio == -1.0 and delta_time > stall_time:
 				print("Stall item in list. Remove item({}) ".format(full_hash_magnet))
 				completed_dic[full_hash_magnet]={'title':item['name'],'status':"stall"}
 				remove_torrent_at_server(conn, item['id'])
 				delete_flag = 1
 			else:
-				running_dic[full_hash_magnet]={'title':item['name'],'status':status} 
+				running_dic[full_hash_magnet]={'title':item['name'],'status':status}
 				continue;
 	except:
 		return running_dic, completed_dic, max_cnt
@@ -162,10 +158,13 @@ def proc_run(magnet, host=None, t_id=None, t_port=None, t_pass=None):
 	add_cnt = 0
 
 	if (delete_flag == 0) and ((len(item_list) - del_cnt) < max_cnt) and (not magnet in running_dic):
+		magnet = "magnet:?xt=urn:btih:"+magnet
 		print("[ {} ] Add magnet to server ({})".format(datetime.today(), magnet))
 		add_magnet_to_server(conn, magnet)
 		add_cnt += 1
-	
+	else:
+		print("running magent : {}".format(magnet))
+
 	release_for_connect(conn)
 	return running_dic, completed_dic, (len(item_list) - del_cnt + add_cnt)
 
